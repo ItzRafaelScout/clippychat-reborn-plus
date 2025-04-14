7dde1abe42521388610ecc5b407f81c715f615ac
ItzRafaelScout <rafaelaltrafaelduartealt@gmail.com>
2025-04-14 19:04:15 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -20,15 +20,56 @@ io.on('connection', (socket) => {
 
     socket.on('login', (data) => {
         if (data.nickname && data.nickname.length <= config.nameLimit) {
+            // Get current users in the room to send to the new user
+            const roomUsers = [];
+            users.forEach((user, id) => {
+                if (user.room === (data.room || 'default') && id !== socket.id) {
+                    roomUsers.push({
+                        id,
+                        nickname: user.nickname,
+                        agent: user.agent,
+                        position: user.position
+                    });
+                }
+            });
+
+            // Generate random position for new user
+            const position = {
+                x: Math.floor(Math.random() * 800) + 50,
+                y: Math.floor(Math.random() * 400) + 50
+            };
+
+            // Store user data
             users.set(socket.id, {
                 nickname: data.nickname,
                 room: data.room || 'default',
-                agent: data.agent || 'clippy'
+                agent: data.agent || 'clippy',
+                position
             });
+
+            // Join the room
             socket.join(data.room || 'default');
-            io.to(data.room || 'default').emit('userJoined', {
+            
+            // Inform others about the new user
+            socket.to(data.room || 'default').emit('userJoined', {
+                id: socket.id,
                 nickname: data.nickname,
-                agent: data.agent || 'clippy'
+                agent: data.agent || 'clippy',
+                position
+            });
+
+            // Send existing users to the new user
+            socket.emit('currentUsers', roomUsers);
+        }
+    });
+
+    socket.on('updatePosition', (position) => {
+        const user = users.get(socket.id);
+        if (user) {
+            user.position = position;
+            socket.to(user.room).emit('agentMoved', {
+                id: socket.id,
+                position
             });
         }
     });
@@ -38,6 +79,7 @@ io.on('connection', (socket) => {
             const user = users.get(socket.id);
             if (user) {
                 io.to(user.room).emit('message', {
+                    id: socket.id,
                     nickname: user.nickname,
                     message: data.message,
                     agent: user.agent
@@ -51,7 +93,8 @@ io.on('connection', (socket) => {
             const user = users.get(socket.id);
             if (user) {
                 user.agent = newAgent;
-                io.to(user.room).emit('agentChanged', {
+                socket.to(user.room).emit('agentChanged', {
+                    id: socket.id,
                     nickname: user.nickname,
                     agent: newAgent
                 });
@@ -62,7 +105,10 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         const user = users.get(socket.id);
         if (user) {
-            io.to(user.room).emit('userLeft', user.nickname);
+            socket.to(user.room).emit('userLeft', {
+                id: socket.id,
+                nickname: user.nickname
+            });
             users.delete(socket.id);
         }
     });
